d2f95b221fe1cb93bd2bb4147f16bc1b262ac4c7
d.vorobyev1 <d.vorobyev1@tinkoff.ru>
2022-01-16 15:44:19 +0300

    List.Reverse() way

---
+++ ReverseSinglyLinkedList/ReverseSinglyLinkedList/LinkedList.cs
@@ -0,0 +1,40 @@
+﻿namespace ReverseSinglyLinkedList
+{
+    public class Solution {
+        public ListNode ReverseList(ListNode head) {
+
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+
+            var List = new List<ListNode>();
+            ListNode? curr = head;
+            while (curr != null)
+            {
+                List.Add(curr);
+                curr = curr.next;
+            }
+
+            List.Reverse();
+            for (int i = 0; i < List.Count - 1; i++)
+            {
+                List[i].next = List[i + 1];
+            }
+
+            List[^1].next = null;
+
+            return List.First();
+        }
+    }
+
+    public class ListNode {
+        public int val { get; set; }
+        public ListNode? next { get; set; }
+        
+        public ListNode(int val, ListNode? next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
