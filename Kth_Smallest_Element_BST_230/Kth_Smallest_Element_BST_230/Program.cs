a5d5c7b682a24da83a9dc4091a28d6dc8a5bd6e4
d.vorobyev1 <d.vorobyev1@tinkoff.ru>
2022-01-30 14:26:09 +0300

    [LC-230] in-order traversal with array

--- Kth_Smallest_Element_BST_230/Kth_Smallest_Element_BST_230/Program.cs
+++ Kth_Smallest_Element_BST_230/Kth_Smallest_Element_BST_230/Program.cs
@@ -1,17 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 var node = new TreeNode(4, new TreeNode(2), new TreeNode(5));
-var k = 1;
-var currentNodeIndex = 0;
-var value = 0;
+const int k = 1;
 
-TreeNode.InOrderTraversal(node, val =>
-{
-    currentNodeIndex++;
-    if (currentNodeIndex == k)
-    {
-        value = val;
-    }
-});
+var values = new List<int>();
 
-Console.WriteLine(value);
+TreeNode.InOrderTraversal(node, val => values.Add(val));
+
+Console.WriteLine(values[k-1]);
