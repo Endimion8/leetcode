1c266663e4992e5edda12399eef828da3a316aee
d.vorobyev1 <d.vorobyev1@tinkoff.ru>
2022-01-30 14:22:49 +0300

    [LC-230] recursive without array

---
+++ Kth_Smallest_Element_BST_230/Kth_Smallest_Element_BST_230/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var node = new TreeNode(4, new TreeNode(2), new TreeNode(5));
+var k = 1;
+var currentNodeIndex = 0;
+var value = 0;
+
+TreeNode.InOrderTraversal(node, val =>
+{
+    currentNodeIndex++;
+    if (currentNodeIndex == k)
+    {
+        value = val;
+    }
+});
+
+Console.WriteLine(value);
---
+++ Kth_Smallest_Element_BST_230/Kth_Smallest_Element_BST_230/TreeNode.cs
@@ -0,0 +1,24 @@
+﻿
+public class TreeNode
+{
+    public int val;
+    public TreeNode? left;
+    public TreeNode? right;
+
+    public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    public static void InOrderTraversal(TreeNode? node, Action<int> action)
+    {
+        if (node is not null)
+        {
+            InOrderTraversal(node.left, action);
+            action(node.val);
+            InOrderTraversal(node.right, action);
+        }
+    }
+}
