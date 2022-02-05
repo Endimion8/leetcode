6209fa3dd2c3b16c9f08080a958d84c03035c21c
d.vorobyev1 <d.vorobyev1@tinkoff.ru>
2022-02-05 22:25:13 +0300

    [340] flatten whole list in constructor

---
+++ FlattenNestedListIterator.341/FlattenNestedListIterator.341/NestedInteger.cs
@@ -0,0 +1,24 @@
+﻿namespace FlattenNestedListIterator._341;
+
+public class NestedInteger
+{
+    // @return true if this NestedInteger holds a single integer, rather than a nested list.
+    public bool IsInteger()
+    {
+        return true;
+    }
+
+    // @return the single integer that this NestedInteger holds, if it holds a single integer
+    // Return null if this NestedInteger holds a nested list
+    public int GetInteger()
+    {
+        return 0;
+    }
+
+    // @return the nested list that this NestedInteger holds, if it holds a nested list
+    // Return null if this NestedInteger holds a single integer
+    public IList<NestedInteger> GetList()
+    {
+        return new List<NestedInteger>();
+    }
+}
---
+++ FlattenNestedListIterator.341/FlattenNestedListIterator.341/NestedIterator.cs
@@ -0,0 +1,46 @@
+﻿namespace FlattenNestedListIterator._341;
+
+public class NestedIterator
+{
+    private int Position { get; set; } = -1;
+    private List<int> FlattenedList { get; }
+
+    public NestedIterator(IList<NestedInteger> nestedList) {
+        FlattenedList = new List<int>();
+        
+        foreach (var nestedInteger in nestedList)
+        {
+            FlattenList(nestedInteger);
+        }
+    }
+
+    private void FlattenList(NestedInteger nestedInteger)
+    {
+        if (nestedInteger.IsInteger())
+        {
+            FlattenedList.Add(nestedInteger.GetInteger());
+            return;
+        }
+
+        var list = nestedInteger.GetList();
+        foreach (var next in list)
+        {
+            FlattenList(next);
+        }
+    }
+
+    public bool HasNext()
+    {
+        return Position < FlattenedList.Count - 1;
+    }
+
+    public int Next() {
+        if (HasNext())
+        {
+            Position++;
+            return FlattenedList[Position];
+        }
+
+        return Int32.MinValue;
+    }
+}
