bacdbd18bd3a51a5ebd2d29fbc7c8c026e1f621d
d.vorobyev1 <d.vorobyev1@tinkoff.ru>
2022-02-27 20:14:02 +0300

    [LongestPalindromicSubstring.5] O(n^3) brute force (Time Limit Exceeded)

---
+++ LongestPalindromicSubstring.5/LongestPalindromicSubstring.5/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+const string inStr = "xabax";
+var longestPalindrome = "";
+
+for (int i = 0; i < inStr.Length; i++)
+{
+    for (int length = 1; length <= inStr.Length - i; length++)
+    {
+        var substring = inStr.Substring(i, length);
+        var equals = substring == new string(substring.Reverse().ToArray());
+        if (equals && substring.Length > longestPalindrome.Length)
+        {
+            longestPalindrome = substring;
+        }
+    }
+}
+
+Console.WriteLine(longestPalindrome);
