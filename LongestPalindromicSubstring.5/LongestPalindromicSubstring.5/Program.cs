2891cebbb47065742a29be789403ee4f35640408
d.vorobyev1 <d.vorobyev1@tinkoff.ru>
2022-02-27 21:35:56 +0300

    [LongestPalindromicSubstring.5] O(n^2) approach (high low)

--- LongestPalindromicSubstring.5/LongestPalindromicSubstring.5/Program.cs
+++ LongestPalindromicSubstring.5/LongestPalindromicSubstring.5/Program.cs
@@ -1,30 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
-const string inStr = "xabay";
-var maxLength = 1;
-var startPosition = 0;
+const string s = "jsfpmgkuxqnmtruslzgyvexhqjoamvyuyedhybqqcjhhhgmwqudgldvspgugibdsqfhucpfcqzriqqusvspgbqhgkswlzdkytyqphexemwxpduxplkquvgvhefsxubjluopighxbpscekijrqjhcgmqcuoczwbvueuviyfokdoqqsckjdorsettkkpiyyxxdsfczyhkyxlvrmhvflqbvlrukqcplbxnyokdxvhubsisxrodolmpmkdczavqlsnrggffagoddaldlcexwvozjxxdjtfjrfciwpacpbajcpmgfpefngqfbzehaaqyfvthtrbhkzrzqmzdcgrkezpqgbqjembeqaziuubbvdfpfyqanilcjggkudsyigiqgrcmauyugyhepvduudvpehyguyuamcrgqigiysdukggjclinaqyfpfdvbbuuizaqebmejqbgqpzekrgcdzmqzrzkhbrthtvfyqaahezbfqgnfepfgmpcjabpcapwicfrjftjdxxjzovwxecldladdogaffggrnslqvazcdkmpmlodorxsisbuhvxdkoynxblpcqkurlvbqlfvhmrvlxykhyzcfsdxxyyipkkttesrodjkcsqqodkofyivueuvbwzcoucqmgchjqrjikecspbxhgipouljbuxsfehvgvuqklpxudpxwmexehpqytykdzlwskghqbgpsvsuqqirzqcfpcuhfqsdbigugpsvdlgduqwmghhhjcqqbyhdeyuyvmaojqhxevygzlsurtmnqxukgmpfsj";
+Console.WriteLine(getLongestPalindrome(s));
 
-for (int i = 0; i < inStr.Length; i++)
+string getLongestPalindrome(string s)
 {
-    for (int j = i; j < inStr.Length; j++)
+    var n = s.Length;
+    if (n < 2)
     {
-        var isPalindrome = true;
-        var length = (j - i + 1);
-        var middle = length / 2;
-        for (int k = 0; k < middle; k++)
+        return s;
+    }
+    var maxLength = 1;
+    var startPosition = 0;
+
+    int low, high;
+    for (int i = 0; i < s.Length; i++)
+    {
+        low = i - 1;
+        high = i + 1;
+
+        while (high < n && s[i] == s[high])
         {
-            if (inStr[i + k] != inStr[j - k])
-            {
-                isPalindrome = false;
-            }
+            high++;
+        }
+        while (low >= 0 && s[low] == s[i])
+        {
+            low--;
         }
 
-        if (isPalindrome && length > maxLength)
+        while (low >= 0 && high < n && s[low] == s[high])
+        {
+            low--;
+            high++;
+        }
+
+        var length = high - low - 1;
+        if (length > maxLength)
         {
             maxLength = length;
-            startPosition = i;
+            startPosition = low + 1;
         }
     }
-}
 
-Console.WriteLine(inStr.Substring(startPosition, maxLength));
-Console.WriteLine(inStr[startPosition..(startPosition + maxLength)]);
+    return s[startPosition..(startPosition + maxLength)];
+}
