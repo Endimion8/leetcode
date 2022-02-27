29c3d0e1517f5fa5fc2ec2094cc06d667f27f568
d.vorobyev1 <d.vorobyev1@tinkoff.ru>
2022-02-27 21:12:13 +0300

    [LongestPalindromicSubstring.5] O(n^3) brute force second version (Time Limit Exceeded also)

--- LongestPalindromicSubstring.5/LongestPalindromicSubstring.5/Program.cs
+++ LongestPalindromicSubstring.5/LongestPalindromicSubstring.5/Program.cs
@@ -1,18 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-const string inStr = "xabax";
-var longestPalindrome = "";
+const string inStr = "xabay";
+var maxLength = 1;
+var startPosition = 0;
 
 for (int i = 0; i < inStr.Length; i++)
 {
-    for (int length = 1; length <= inStr.Length - i; length++)
+    for (int j = i; j < inStr.Length; j++)
     {
-        var substring = inStr.Substring(i, length);
-        var equals = substring == new string(substring.Reverse().ToArray());
-        if (equals && substring.Length > longestPalindrome.Length)
+        var isPalindrome = true;
+        var length = (j - i + 1);
+        var middle = length / 2;
+        for (int k = 0; k < middle; k++)
         {
-            longestPalindrome = substring;
+            if (inStr[i + k] != inStr[j - k])
+            {
+                isPalindrome = false;
+            }
+        }
+
+        if (isPalindrome && length > maxLength)
+        {
+            maxLength = length;
+            startPosition = i;
         }
     }
 }
 
-Console.WriteLine(longestPalindrome);
+Console.WriteLine(inStr.Substring(startPosition, maxLength));
+Console.WriteLine(inStr[startPosition..(startPosition + maxLength)]);
