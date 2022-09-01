402d503740ad1757856a414b13e9d69b4132fcee
d.vorobyev1 <d.vorobyev1@tinkoff.ru>
2022-09-01 21:09:01 +0300

    ShortenUnixPath

---
+++ ShortUnixPath/ShortUnixPath/Program.cs
@@ -0,0 +1,37 @@
+﻿// Необходимо написать функцию, которая получает строку с абсолютным UNIX путем
+// и возвращает укороченную версию, удалив все ненужное
+//
+// Input: "/foo/../test/../test/../foo//bar/./baz"
+// Output: "/foo/bar/baz"
+// Пояснения
+//
+//     .. - возврат на директорию выше
+//     . - означает текущую директорию, по сути просто мусор
+//     /// - просто мусор
+//     нельзя выйти выше корневой директории
+
+// https://wiki2.tcsbank.ru/pages/viewpage.action?pageId=1415519426
+
+string ShortenPath(string path)
+{
+    var segments = path.Split('/').Where(x => x.Length > 0 && x != ".");
+    var stack = new Stack<string>();
+    foreach (var segment in segments)
+    {
+        if (segment == "..")
+        {
+            stack.TryPop(out _);
+        }
+        else
+        {
+            stack.Push(segment);
+        }
+    }
+
+    return "/" + string.Join('/', stack.ToArray().Reverse());
+}
+
+
+Console.WriteLine(ShortenPath("/foo/../test/../test/../foo//bar/./baz"));
+Console.WriteLine(ShortenPath("///////"));
+Console.WriteLine(ShortenPath("/../../../"));
